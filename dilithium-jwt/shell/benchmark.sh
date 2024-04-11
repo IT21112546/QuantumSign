6ee4ec3d179b9cdb0d31d1cbf997254cd33eaa93
meeranh <themeeranhassan@gmail.com>
2024-04-11 06:29:33 +0530

    Changed verification-generation benchmarking order

--- dilithium-jwt/shell/benchmark.sh
+++ dilithium-jwt/shell/benchmark.sh
@@ -2,7 +2,7 @@
 
 echo ""
 echo "Benchmarking memory usage..."
-./dilithium_verify && ./ed25519_verify && ./rsa_verify && ./dilithium_keygen && ./ed25519_keygen && ./rsa_keygen
+./dilithium_keygen && ./ed25519_keygen && ./rsa_keygen && ./dilithium_verify && ./ed25519_verify && ./rsa_verify
 
 echo ""
 echo "Benchmarking performance..."
