81512581d8e89a31c11833366890697b60c536d9
meeranh <themeeranhassan@gmail.com>
2024-04-25 17:07:16 +0530

    Modified to clear terminal after input

--- kyber-auth/entrypoint.sh
+++ kyber-auth/entrypoint.sh
@@ -11,14 +11,17 @@ read -p "Enter your choice (1 or 2): " choice
 # Process the user input
 case $choice in
     1)
+				clear
         printf "Starting the client...\n\n"
         ./run_client
         ;;
     2)
+				clear
         printf "Starting the server...\n\n"
         ./run_server
         ;;
     *)
+				clear
         printf "Invalid choice\n\n"
         exit 1
         ;;
