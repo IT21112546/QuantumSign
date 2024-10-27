6f66872f75ad63c2c0b437bdb44811dda3008331
meeranh <themeeranhassan@gmail.com>
2024-10-27 19:00:31 +0530

    chore: factored https:// to be set in env vars

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   const router = useRouter()
   const clientId = process.env.NEXT_CLIENT_ID || '26d9b87b-8b44-4bc3-8f10-2b966258b136'
-  const apiHost = process.env.NEXT_PUBLIC_SSO_URL || 'localhost:3000'
+  const apiHost = process.env.NEXT_PUBLIC_SSO_URL || 'http://localhost:3000'
 
   const handleLogin = () => {
-    router.push(`http://${apiHost}/login?client-id=${clientId}`)
+    router.push(`${apiHost}/login?client-id=${clientId}`)
   }
 
   return (
